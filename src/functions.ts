1dfc4588c6b39ef818f7276fff2eaab636fe119e
I546967 <jenny.park@sap.com>
2021-04-27 14:54:33 -0700

    typescript functions

---
+++ src/functions.ts
@@ -0,0 +1,29 @@
+// Functions (optional and default parameters)
+// function types style1
+function sum(a: number, b: number = 0): number {
+    return a + b;
+}
+
+sum(1, 2)
+sum(1);
+
+// function types style2
+type MyFunc = (a: number, b: number) => number;
+const sum2: MyFunc = (a, b) => a + b;
+
+
+// Unknown number of arguments
+function sumEverything(arg1: string, arg2: boolean, ...numbers: number[]): number {
+    return numbers.reduce((result, num) => result + num, 0)
+}
+
+// Overloads
+
+function calcArea(width: number, height: number): number;
+function calcArea(length: number): number;
+function calcArea(...args: number[]): number {
+    if (args.length === 2) {
+        return args[0] * args[1];
+    }
+    return Math.pow(args[0], 2);
+} 
