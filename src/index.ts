0145956111ba274a787a58a3473bb8165ea4cd51
I546967 <jenny.park@sap.com>
2021-04-27 12:23:37 -0700

    create a mudule and export and import it from different files

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
-function multiply(a: number, b: number) {
-    return a * b
-}
+import multiply, { multiplyByTwo as mBy2 } from "./multiply";
 
 const a = 3;
 const b = 2;
 
 console.log(`${a} * ${b} = ${multiply(a, b)}`);
+mBy2(2);
+
+
+// ways2
+// import * as multiplyModule from './multiply';
+
+// const a = 3;
+// const b = 2;
+
+// console.log(`${a} * ${b} = ${multiplyModule.default(a, b)}`);
+// multiplyModule.multiplyByTwo(2);
---
+++ src/multiply.ts
@@ -0,0 +1,7 @@
+export default function multiply(a: number, b: number) {
+    return a * b
+}
+
+export function multiplyByTwo(num: number) {
+    return multiply(num, 2);
+}
